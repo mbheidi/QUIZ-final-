6740171dcb9cca229b05a39d8dbdb2c59f8954cb
mbheidi <78099151+mbheidi@users.noreply.github.com>
2021-03-20 23:26:19 -0700

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -1,2 +1,65 @@
-
-nnnn
+$(document).ready(function(){
+  $("#b1").click(function(){
+    $("#p1").fadeIn();
+  });
+  
+      $("#b2").click(function(){
+        $("#p2").fadeIn();
+        
+         });
+  
+  
+  $("b3").click(function(){
+    $("#p3).fadeIn();
+      
+      });
+ 
+    
+   $("b4").click(function(){
+    $("#p4).fadeIn();
+      
+      });
+     
+     
+    $("b5").click(function(){
+    $("#p5).fadeIn();
+      
+      });
+      
+      
+     $("b6").click(function(){
+    $("#p6).fadeIn();
+      
+      });  
+      
+       
+       
+    $("b7").click(function(){
+    $("#p7).fadeIn();
+      
+      });
+      
+      
+    $("b8").click(function(){
+    $("#p8).fadeIn();
+      
+      }); 
+      
+      
+   
+    $("b9").click(function(){
+    $("#p9).fadeIn();
+      
+      }); 
+       
+      
+      $("b10").click(function(){
+    $("#p10).fadeIn();
+      
+      });  
+      
+      
+      
+      
+      
+      
