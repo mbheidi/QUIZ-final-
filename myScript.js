45b08e3002cd8511c784860b4e4886d67eb67d4e
mbheidi <78099151+mbheidi@users.noreply.github.com>
2021-03-20 23:32:04 -0700

    Update myScript.js

--- myScript.js
+++ myScript.js
@@ -9,52 +9,52 @@ $(document).ready(function(){
          });
   
   
-  $("b3").click(function(){
-    $("#p3).fadeIn();
+  $("#b3").click(function(){
+    $("#p3").fadeIn();
       
       });
  
     
-   $("b4").click(function(){
-    $("#p4).fadeIn();
+   $("#b4").click(function(){
+    $("#p4").fadeIn();
       
       });
      
      
-    $("b5").click(function(){
-    $("#p5).fadeIn();
+    $("#b5").click(function(){
+    $("#p5").fadeIn();
       
       });
       
       
-     $("b6").click(function(){
-    $("#p6).fadeIn();
+     $("#b6").click(function(){
+    $("#p6").fadeIn();
       
       });  
       
        
        
-    $("b7").click(function(){
-    $("#p7).fadeIn();
+    $("#b7").click(function(){
+    $("#p7").fadeIn();
       
       });
       
       
-    $("b8").click(function(){
-    $("#p8).fadeIn();
+    $("#b8").click(function(){
+    $("#p8").fadeIn();
       
       }); 
       
       
    
-    $("b9").click(function(){
-    $("#p9).fadeIn();
+    $("#b9").click(function(){
+    $("#p9").fadeIn();
       
       }); 
        
       
-      $("b10").click(function(){
-    $("#p10).fadeIn();
+      $("#b10").click(function(){
+    $("#p10").fadeIn();
       
       });  
       
